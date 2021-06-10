53e78a0a96eb88a6df94eaa823428f9aba07dc58
Vasif <vasifm1@gmail.com>
2021-06-10 11:06:53 +0530

    Added javascript programs

---
+++ javascript programs/basic.js
@@ -0,0 +1,11 @@
+var p_name="Vasif";
+var age=24;
+
+console.log("Hi im "+p_name,age+" years old");
+
+var state="Kerala";
+var active_cases="15000";
+var cured=2000;
+var death=200;
+
+console.log(`in ${state} ${active_cases} new cases and ${cured} death ${death} has been reported`);
---
+++ javascript programs/swapping.js
@@ -0,0 +1,10 @@
+var num1=10,num2=20;
+
+console.log(`values b4 swaping num1=${num1} and num2=${num2}`);
+
+// var temp=num1
+// num1=num2;
+// num2=temp;
+
+
+console.log(`values after swaping num1=${num1} and num2=${num2}`);
