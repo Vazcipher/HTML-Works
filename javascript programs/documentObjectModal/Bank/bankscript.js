b7528df9edc1dee843d32a698cc7b9193e5a313f
vasif <vasifm1@gmail.com>
2021-07-11 00:54:15 +0530

    country data and weather app

--- javascript programs/documentObjectModal/Bank/bankscript.js
+++ javascript programs/documentObjectModal/Bank/bankscript.js
@@ -1,4 +1,4 @@
-function getAccountDetails() {
+function addAccount() {
     let name=nme.value;
     let account_number=acc_no.value;
     let balance=bal.value;
@@ -13,6 +13,97 @@ function getAccountDetails() {
     else{
         localStorage.setItem(account.account_number,JSON.stringify(account))
         alert("Account added success")
-        location.reload(true            )
+        location.href="./login.html"
     }
 }
+
+function login() {
+    let user_name=uname.value;
+    let password=pwd.value;
+    let login_account={user_name,password}
+    console.log(login_account);
+
+    let user=JSON.parse(localStorage.getItem(user_name))
+    console.log(user);
+    if(user_name in localStorage){
+        if(user.password== password){
+            alert("Login Success")
+            sessionStorage.setItem("user",user_name)
+            location.href="./home.html"
+        }
+        else{
+            error.innerHTML="*please enter a valid password"
+        }
+    }
+    else{
+        error.innerHTML="*invalid username"
+    }
+}
+
+function balanceEnq() {
+    let user=sessionStorage.getItem("user");
+    console.log(user);
+    let account=JSON.parse(localStorage.getItem(user))
+    alert(`Your account balance is: ${account.balance}`)
+}
+
+var req=sessionStorage.getItem("user")
+if(req){
+    litem.innerHTML=`Logout ${req}`
+}
+
+
+function logout() {
+    sessionStorage.removeItem("user")
+    location.href="login.html"
+}
+
+function fundTransfer() {
+    let to_account=toacc.value;
+    let confirm_acc=cacc.value;
+    let amount=amnt.value;
+
+    let loged_user=JSON.parse(localStorage.getItem(req))
+    console.log(loged_user);
+
+    if(loged_user.balance<amount){
+        alert("insufficant balance")
+    }
+    else{
+        loged_user.balance=Number(loged_user.balance)-Number(amount)
+        localStorage.setItem(loged_user.account_number,JSON.stringify(loged_user))
+        if(to_account in localStorage){
+            var user=JSON.parse(localStorage.getItem(to_account))
+            user.balance=Number(user.balance)+Number(amount)
+            localStorage.setItem(user.account_number,JSON.stringify(user))
+            alert("Transaction complete")
+        }
+        else{
+            alert("This to account not found")
+        }
+    }
+
+}
+
+
+
+// function fundTransfer() {
+//     let to_account=toacc.value;
+//     let confirm_acc=cacc.value;
+//     let amount=amnt.value
+//     let loged_user=JSON.parse(localStorage.getItem(req))
+
+//     if(loged_user.balance<amount){
+//         alert("Insufficiant balance")
+//     }
+//     else{
+//         loged_user.balance=Number(loged_user.balance)-Number(amount);
+//         localStorage.setItem(loged_user.account_number,JSON.stringify(loged_user))
+//         if(to_account in localStorage){
+//             let user=JSON.parse(localStorage.getItem(to_account))
+//             user.balance=Number(user.balance)+Number(amount)
+//             localStorage.setItem(user.account_number,JSON.stringify(user))
+//             alert("Transaction completed")
+//         }
+//     }
+// }
